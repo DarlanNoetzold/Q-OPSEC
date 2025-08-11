4f9e137045665667f0ded7b409fe5a4e30c2f865
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-10 23:09:33 -0300

    Create ContextApiApplicationTests.java

---
+++ context_api/src/test/java/tech/noetzold/context_api/ContextApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.context_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
