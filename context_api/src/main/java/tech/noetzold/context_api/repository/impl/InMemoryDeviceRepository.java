e9b451d7189303c6b571b00f48a7113ee1070f1a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:43 -0300

    Update InMemoryDeviceRepository.java

--- context_api/src/main/java/tech/noetzold/context_api/repository/impl/InMemoryDeviceRepository.java
+++ context_api/src/main/java/tech/noetzold/context_api/repository/impl/InMemoryDeviceRepository.java
@@ -1,4 +1,20 @@
 package tech.noetzold.context_api.repository.impl;
 
-public class InMemoryDeviceRepository {
+import org.springframework.stereotype.Repository;
+import tech.noetzold.context_api.model.DeviceMeta;
+import tech.noetzold.context_api.repository.DeviceRepository;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class InMemoryDeviceRepository implements DeviceRepository {
+    private static final Map<String, DeviceMeta> DB = Map.of(
+            "dev-123", new DeviceMeta("Android 10", "IoT", "enabled", "compliant")
+    );
+
+    @Override
+    public Optional<DeviceMeta> findByDeviceId(String deviceId) {
+        return Optional.ofNullable(DB.getOrDefault(deviceId, new DeviceMeta("unknown","unknown","unknown","unknown")));
+    }
 }
