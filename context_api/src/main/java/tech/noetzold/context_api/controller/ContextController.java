9d6b93a14cb9d2655d17331f815c01ae456cdff0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:36 -0300

    Update ContextController.java

--- context_api/src/main/java/tech/noetzold/context_api/controller/ContextController.java
+++ context_api/src/main/java/tech/noetzold/context_api/controller/ContextController.java
@@ -1,4 +1,23 @@
 package tech.noetzold.context_api.controller;
 
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.context_api.model.EnrichRequest;
+import tech.noetzold.context_api.model.EnrichResponse;
+import tech.noetzold.context_api.service.ContextEnrichmentService;
+
+@RestController
+@RequestMapping("/context")
 public class ContextController {
+
+    private final ContextEnrichmentService service;
+
+    public ContextController(ContextEnrichmentService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/enrich")
+    public EnrichResponse enrich(@RequestBody EnrichRequest req, ServerHttpRequest httpReq) {
+        return service.enrichAll(req, httpReq);
+    }
 }
