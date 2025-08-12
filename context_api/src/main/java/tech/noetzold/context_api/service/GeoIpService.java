32edf93c225825c49c832ef3cb47ae106766aceb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:54 -0300

    Update GeoIpService.java

--- context_api/src/main/java/tech/noetzold/context_api/service/GeoIpService.java
+++ context_api/src/main/java/tech/noetzold/context_api/service/GeoIpService.java
@@ -1,4 +1,13 @@
 package tech.noetzold.context_api.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class GeoIpService {
+    public Optional<String> lookupCountry(String ip) {
+        if (ip != null && ip.startsWith("192.168.")) return Optional.of("PrivateNet");
+        return Optional.ofNullable(ip).map(x -> "Unknown");
+    }
 }
