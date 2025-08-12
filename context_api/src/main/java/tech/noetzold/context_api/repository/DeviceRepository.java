5ab0a9a600fbfaa8a0ee26ffc44520a60a16f876
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:41 -0300

    Update DeviceRepository.java

--- context_api/src/main/java/tech/noetzold/context_api/repository/DeviceRepository.java
+++ context_api/src/main/java/tech/noetzold/context_api/repository/DeviceRepository.java
@@ -1,4 +1,7 @@
 package tech.noetzold.context_api.repository;
 
-public class DeviceRepository {
+import java.util.Optional;
+
+public interface DeviceRepository {
+    Optional<tech.noetzold.context_api.model.DeviceMeta> findByDeviceId(String deviceId);
 }
