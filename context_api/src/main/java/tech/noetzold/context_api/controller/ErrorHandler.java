63a64da86c689eed596a5d4189b153ee51b15318
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:38 -0300

    Update ErrorHandler.java

--- context_api/src/main/java/tech/noetzold/context_api/controller/ErrorHandler.java
+++ context_api/src/main/java/tech/noetzold/context_api/controller/ErrorHandler.java
@@ -1,4 +1,25 @@
 package tech.noetzold.context_api.controller;
 
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+@RestControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(TimeoutException.class)
+    @ResponseStatus(HttpStatus.GATEWAY_TIMEOUT)
+    public Map<String,Object> handleTimeout(TimeoutException ex) {
+        return Map.of(
+                "code","UPSTREAM_TIMEOUT",
+                "message","Risk or Confidentiality service timed out",
+                "retry_after_ms",200,
+                "trace_id", MDC.get("trace_id")
+        );
+    }
 }
