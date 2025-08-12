81a413039578aa0273e45888d32705ca284133f5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:18:10 -0300

    Update DestinationContext.java

--- context_api/src/main/java/tech/noetzold/context_api/model/DestinationContext.java
+++ context_api/src/main/java/tech/noetzold/context_api/model/DestinationContext.java
@@ -1,4 +1,13 @@
 package tech.noetzold.context_api.model;
 
-public class DestinationContext {
-}
+import java.util.List;
+
+public record DestinationContext(
+        String ip,
+        String service_id,
+        String service_type,
+        String security_policy,
+        String security_status,
+        String os_version,
+        List<String> allowed_protocols
+) {}
