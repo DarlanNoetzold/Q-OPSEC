db8d75696f0980c0c31f2a96a3218eff1c8226d7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:35 -0300

    Update WebClientConfig.java

--- context_api/src/main/java/tech/noetzold/context_api/config/WebClientConfig.java
+++ context_api/src/main/java/tech/noetzold/context_api/config/WebClientConfig.java
@@ -1,4 +1,27 @@
 package tech.noetzold.context_api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    @Bean
+    public WebClient riskWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://risk-service:8082")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    @Bean
+    public WebClient confWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://conf-service:8083")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
 }
