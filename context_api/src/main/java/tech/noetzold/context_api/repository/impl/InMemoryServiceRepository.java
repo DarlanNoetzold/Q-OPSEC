61e2914e39c49cff65b31a420a60103dd0318330
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-12 12:17:46 -0300

    Update InMemoryServiceRepository.java

--- context_api/src/main/java/tech/noetzold/context_api/repository/impl/InMemoryServiceRepository.java
+++ context_api/src/main/java/tech/noetzold/context_api/repository/impl/InMemoryServiceRepository.java
@@ -1,4 +1,25 @@
 package tech.noetzold.context_api.repository.impl;
 
-public class InMemoryServiceRepository {
+import org.springframework.stereotype.Repository;
+import tech.noetzold.context_api.model.ServiceMeta;
+import tech.noetzold.context_api.repository.ServiceRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class InMemoryServiceRepository implements ServiceRepository {
+
+    @Override
+    public Optional<ServiceMeta> findByServiceIdAndHost(String serviceId, String host, String path) {
+        // Stubbed example
+        return Optional.of(new ServiceMeta(
+                "10.0.0.5",
+                "database",
+                "high",
+                "monitored",
+                "Linux 5.10",
+                List.of("TLS1.3")
+        ));
+    }
 }
