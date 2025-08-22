5b6d8ed47d765029a591d536fff2dcf259118fd8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 09:36:12 -0300

    Update InterceptService.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/service/InterceptService.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/service/InterceptService.java
@@ -1,4 +1,27 @@
 package tech.noetzold.interceptor_api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.interceptor_api.client.ContextApiClient;
+
+// service/InterceptService.java
+@Service
 public class InterceptService {
+    @Autowired
+    private ContextApiClient contextApiClient;
+
+    public InterceptResponse intercept(InterceptRequest req) {
+        ContextRequest ctxReq = new ContextRequest();
+        ctxReq.setSourceId(req.getSourceId());
+        ctxReq.setDestinationId(req.getDestinationId());
+        ctxReq.setContent(req.getMessage());
+        ctxReq.setMetadata(req.getMetadata());
+
+        ContextResponse context = contextApiClient.getContext(ctxReq);
+
+        InterceptResponse resp = new InterceptResponse();
+        resp.setStatus("OK");
+        resp.setContext(context);
+        return resp;
+    }
 }
