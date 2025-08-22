f3a3dc458670b3677fa6f6e373b5f1c4a0eb96b1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 10:33:35 -0300

    Update ContextRequest.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/ContextRequest.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/ContextRequest.java
@@ -1,4 +1,15 @@
 package tech.noetzold.interceptor_api.model;
 
+import lombok.*;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ContextRequest {
+    private String sourceId;
+    private String destinationId;
+    private String content;
+    private Map<String, Object> metadata;
 }
