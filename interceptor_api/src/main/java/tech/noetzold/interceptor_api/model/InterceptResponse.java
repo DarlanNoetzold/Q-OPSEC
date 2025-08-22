f7634fe415c347f04822559a4834652c3c1bdfde
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 10:33:28 -0300

    Update InterceptResponse.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/InterceptResponse.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/InterceptResponse.java
@@ -1,4 +1,12 @@
 package tech.noetzold.interceptor_api.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class InterceptResponse {
+    private String status;
+    private ContextResponse context;
 }
