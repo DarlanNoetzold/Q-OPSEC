f83a1a4050265c8a79313bb166367194826325ff
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 09:36:10 -0300

    Update InterceptController.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/controller/InterceptController.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/controller/InterceptController.java
@@ -1,4 +1,24 @@
 package tech.noetzold.interceptor_api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.interceptor_api.model.InterceptRequest;
+import tech.noetzold.interceptor_api.model.InterceptResponse;
+import tech.noetzold.interceptor_api.service.InterceptService;
+
+@RestController
+@RequestMapping("/intercept")
 public class InterceptController {
+    @Autowired
+    private InterceptService interceptService;
+
+    @PostMapping
+    public ResponseEntity<InterceptResponse> intercept(@RequestBody InterceptRequest req) {
+        InterceptResponse resp = interceptService.intercept(req);
+        return ResponseEntity.ok(resp);
+    }
 }
