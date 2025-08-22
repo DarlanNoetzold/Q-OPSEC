cb68516c20ae23745145282e1f89907e0b991439
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 10:33:31 -0300

    Update InterceptRequest.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/InterceptRequest.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/InterceptRequest.java
@@ -1,4 +1,15 @@
 package tech.noetzold.interceptor_api.model;
 
+import lombok.*;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class InterceptRequest {
+    private String message;
+    private String sourceId;
+    private String destinationId;
+    private Map<String, Object> metadata;
 }
