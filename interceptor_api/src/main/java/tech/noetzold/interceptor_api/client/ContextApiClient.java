c733373ff905ae6a5a2c74beb79545c0b949fc3b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 10:33:02 -0300

    Update ContextApiClient.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/client/ContextApiClient.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/client/ContextApiClient.java
@@ -1,4 +1,19 @@
 package tech.noetzold.interceptor_api.client;
 
+import tech.noetzold.interceptor_api.model.ContextRequest;
+import tech.noetzold.interceptor_api.model.ContextResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class ContextApiClient {
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${context.api.url:http://localhost:8081/context}")
+    private String contextApiUrl;
+
+    public ContextResponse getContext(ContextRequest req) {
+        return restTemplate.postForObject(contextApiUrl, req, ContextResponse.class);
+    }
 }
