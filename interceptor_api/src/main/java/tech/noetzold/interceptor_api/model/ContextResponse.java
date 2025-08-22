e301db68a317333d8271b126652a571117266118
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-22 10:33:33 -0300

    Update ContextResponse.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/ContextResponse.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/ContextResponse.java
@@ -1,4 +1,15 @@
 package tech.noetzold.interceptor_api.model;
 
+import lombok.*;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ContextResponse {
+    private String source;
+    private String destination;
+    private Map<String, Object> risk;
+    private Map<String, Object> confidentiality;
 }
