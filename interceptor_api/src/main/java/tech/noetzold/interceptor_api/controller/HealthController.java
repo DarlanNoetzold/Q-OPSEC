ca3848314527e41d0595c71f70df9cd85eb870b9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-25 22:52:51 -0300

    Create HealthController.java

---
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package tech.noetzold.interceptor_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+        return ResponseEntity.ok().body("{\"status\":\"ok\"}");
+    }
+}
