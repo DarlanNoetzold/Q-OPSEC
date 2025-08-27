3ee18bec985362ab17de53bf31956033b022c919
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-26 22:04:05 -0300

    Update RestConfig.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/RestConfig.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/RestConfig.java
@@ -1,27 +1,24 @@
 package tech.noetzold.interceptor_api;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
-import java.util.List;
 
 @Configuration
 public class RestConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        var factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
-        factory.setReadTimeout((int) Duration.ofSeconds(5).toMillis());
-
-        RestTemplate rt = new RestTemplate(factory);
-        rt.setInterceptors(List.of((request, body, execution) -> {
-            request.getHeaders().add("X-Interceptor", "interceptor_api");
-            return execution.execute(request, body);
-        }));
-        return rt;
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .additionalInterceptors((request, body, execution) -> {
+                    request.getHeaders().add("X-Interceptor", "interceptor_api");
+                    return execution.execute(request, body);
+                })
+                .build();
     }
 }
