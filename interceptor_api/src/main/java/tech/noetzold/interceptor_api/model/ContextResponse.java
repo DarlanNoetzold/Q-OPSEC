2e4763346719c26b219e6eacc8b2ab95939a22e3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-08-26 22:05:56 -0300

    Update ContextResponse.java

--- interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/ContextResponse.java
+++ interceptor_api/src/main/java/tech/noetzold/interceptor_api/model/ContextResponse.java
@@ -1,15 +1,25 @@
 package tech.noetzold.interceptor_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ContextResponse {
-    private String source;
-    private String destination;
+
+    @JsonProperty("request_id")
+    private String requestId;
+
+    private Map<String, Object> source;
+    private Map<String, Object> destination;
+
+    // Você já usava Map, está ok manter como Map
     private Map<String, Object> risk;
     private Map<String, Object> confidentiality;
 }
