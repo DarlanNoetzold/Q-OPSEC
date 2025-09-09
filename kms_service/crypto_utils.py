666badd04c1b54c3c6d4489d6475d7ef5961ba50
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-09-09 14:36:27 -0300

    Update crypto_utils.py

--- kms_service/crypto_utils.py
+++ kms_service/crypto_utils.py
@@ -1,21 +1,8 @@
-import os, base64
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 
-
 def derive_key(material: bytes, length: int = 32, info: bytes = b"session-key") -> bytes:
-    """
-    Deriva uma chave simétrica fixa (AES-256) a partir de material arbitrário.
-
-    Args:
-        material (bytes): material bruto (ex: chave Kyber/QKD/raw bits).
-        length (int): tamanho da chave final (bytes). Default: 32 (AES-256).
-        info (bytes): contexto da derivação (default: b'session-key').
-
-    Returns:
-        bytes: chave derivada (tamanho fixo).
-    """
     hkdf = HKDF(
         algorithm=hashes.SHA256(),
         length=length,
@@ -24,9 +11,3 @@ def derive_key(material: bytes, length: int = 32, info: bytes = b"session-key") 
         backend=default_backend()
     )
     return hkdf.derive(material)
-
-
-def generate_random_key() -> str:
-    """ Gera uma chave AES-256 aleatória em base64 """
-    raw = os.urandom(32)
-    return base64.b64encode(raw).decode("utf-8")
