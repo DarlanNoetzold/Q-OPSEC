b69275febe36603b3f88adc838dd727dfbbdb69e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-09-15 12:37:26 -0300

    Update ContextController.java

--- context_api/src/main/java/tech/noetzold/context_api/controller/ContextController.java
+++ context_api/src/main/java/tech/noetzold/context_api/controller/ContextController.java
@@ -1,23 +1,36 @@
 package tech.noetzold.context_api.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.context_api.model.EnrichRequest;
 import tech.noetzold.context_api.model.EnrichResponse;
+import tech.noetzold.context_api.model.ContextRecord;
 import tech.noetzold.context_api.service.ContextEnrichmentService;
+import tech.noetzold.context_api.repository.ContextRecordRepository;
 
 @RestController
 @RequestMapping("/context")
 public class ContextController {
 
     private final ContextEnrichmentService service;
+    private final ContextRecordRepository recordRepo;
 
-    public ContextController(ContextEnrichmentService service) {
+    public ContextController(ContextEnrichmentService service,
+                             ContextRecordRepository recordRepo) {
         this.service = service;
+        this.recordRepo = recordRepo;
     }
 
     @PostMapping("/enrich")
     public EnrichResponse enrich(@RequestBody EnrichRequest req, HttpServletRequest httpReq) {
         return service.enrichAll(req, httpReq);
+    }
+
+    @GetMapping("/record")
+    public ResponseEntity<ContextRecord> getRecordByRequestId(@RequestParam("request_id") String requestId) {
+        return recordRepo.findByRequestId(requestId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
