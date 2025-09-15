b8322edcc504699fa0e7345cbc0f0196ed44f99f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-09-15 12:37:28 -0300

    Update ContextRecordRepository.java

--- context_api/src/main/java/tech/noetzold/context_api/repository/ContextRecordRepository.java
+++ context_api/src/main/java/tech/noetzold/context_api/repository/ContextRecordRepository.java
@@ -3,5 +3,8 @@ package tech.noetzold.context_api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.context_api.model.ContextRecord;
 
+import java.util.Optional;
+
 public interface ContextRecordRepository extends JpaRepository<ContextRecord, Long> {
+    Optional<ContextRecord> findByRequestId(String requestId);
 }
