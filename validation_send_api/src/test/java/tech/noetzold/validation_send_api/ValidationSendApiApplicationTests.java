24c3119f0b3fa0eca1c38bdd9a7a515837eb7b7f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-09-16 16:53:20 -0300

    Create ValidationSendApiApplicationTests.java

---
+++ validation_send_api/src/test/java/tech/noetzold/validation_send_api/ValidationSendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.validation_send_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationSendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
