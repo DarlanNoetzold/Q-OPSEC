e58a4f52d65c93e3c81d8a511cd8bbfc243026cd
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-09-16 16:53:26 -0300

    Create ValidationSendApiApplication.java

---
+++ validation_send_api/src/main/java/tech/noetzold/validation_send_api/ValidationSendApiApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.validation_send_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationSendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationSendApiApplication.class, args);
+	}
+
+}
