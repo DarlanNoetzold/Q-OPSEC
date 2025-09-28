9497cf51632be6a097ff2be93f9599b35001250b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-09-28 12:50:35 -0300

    Update negotiator_client.py

--- rl_engine/negotiator_client.py
+++ rl_engine/negotiator_client.py
@@ -2,7 +2,7 @@ import requests
 
 def send_to_handshake(handshake_url: str, payload: dict):
     try:
-        resp = requests.post(handshake_url, json=payload, timeout=3)
+        resp = requests.post(handshake_url, json=payload)
         resp.raise_for_status()
         return resp.json()
     except Exception as e:
