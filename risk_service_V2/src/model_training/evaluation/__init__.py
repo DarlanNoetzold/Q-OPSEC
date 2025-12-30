524508da4a863666936d23ec75bf663f3415d1f8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-12-30 13:56:26 -0300

    Update __init__.py

---
+++ risk_service_V2/src/model_training/evaluation/__init__.py
@@ -0,0 +1,7 @@
+from src.model_training.evaluation.metrics import MetricsCalculator
+from src.model_training.evaluation.visualizer import Visualizer
+
+__all__ = [
+    "MetricsCalculator",
+    "Visualizer",
+]
