cd45c1bd3af11088b282e82c45cd37efe58a76e3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2026-01-03 08:32:49 -0300

    Update __init__.py

---
+++ risk_service_V2/src/api/services/__init__.py
@@ -0,0 +1,5 @@
+from .prediction_service import PredictionService
+from .metrics_service import MetricsService
+from .dataset_service import DatasetService
+
+__all__ = ["PredictionService","MetricsService","DatasetService"]
