193168eda1f521d95a8f4efab8f6ca16e42854c0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2026-02-15 17:48:18 -0300

    Create SwaggerConfig.java

---
+++ context_api/src/main/java/tech/noetzold/context_api/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package tech.noetzold.context_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Context API")
+                        .version("1.0.0")
+                        .description("API completa para gerenciamento de contextos com integração RabbitMQ e PostgreSQL")
+                        .contact(new Contact()
+                                .name("Noetzold Tech")
+                                .email("contato@noetzold.tech")
+                                .url("https://noetzold.tech"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:65534")
+                                .description("Servidor de Desenvolvimento"),
+                        new Server()
+                                .url("https://api.noetzold.tech")
+                                .description("Servidor de Produção")))
+                .tags(List.of(
+                        new Tag().name("Context").description("Operações relacionadas a contextos"),
+                        new Tag().name("Health").description("Endpoints de saúde e monitoramento")));
+    }
+}
