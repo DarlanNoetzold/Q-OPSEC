75b1ce1993df202a346a23232e1f4ceb91bd0243
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2026-03-20 22:31:47 -0300

    Create crypto_utils.py

---
+++ kms_service_v2/crypto_utils.py
@@ -0,0 +1,13 @@
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+def derive_key(material: bytes, length: int = 32, info: bytes = b"session-key") -> bytes:
+    hkdf = HKDF(
+        algorithm=hashes.SHA256(),
+        length=length,
+        salt=None,
+        info=info,
+        backend=default_backend()
+    )
+    return hkdf.derive(material)
